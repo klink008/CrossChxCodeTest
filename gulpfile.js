9be26f14f92f869e2518a0ca2ab5f4b54e0578c7
Kellen <klink008@gmail.com>
2015-08-06 01:25:31 -0400

    Gulp tasks now sequenced.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var clean = require('gulp-clean');
 var concat = require('gulp-concat');
 var ngAnnotate = require('gulp-ng-annotate');
 var html2js = require('gulp-html2js');
+var runSequence = require('run-sequence');
 var karma = require('karma').Server;
 
 var paths = {
@@ -28,11 +29,14 @@ gulp.task('test', function(done) {
     }, done).start();
 });
 
-gulp.task('build', function(){
-    gulp.src(['build/templates.js','src/main/javascripts/*.js'])
+gulp.task('src-build', function(){
+    return gulp.src(['build/templates.js','src/main/javascripts/*.js'])
         .pipe(concat('railroadRoute.js'))
         .pipe(gulp.dest(paths.dest[1]));
-    gulp.src(['node_modules/angular/angular.js',
+});
+
+gulp.task('dep-build', function(){
+    return gulp.src(['node_modules/angular/angular.js',
         'node_modules/underscore/underscore.js',
         'node_modules/restangular/src/restangular.js',
         'node_modules/d3/d3.js'])
@@ -41,7 +45,7 @@ gulp.task('build', function(){
 });
 
 gulp.task('template', function(){
-   gulp.src(paths.html)
+   return gulp.src(paths.html)
        .pipe(html2js({
            outputModuleName: 'templates-main',
            useStrict: true
@@ -50,28 +54,44 @@ gulp.task('template', function(){
        .pipe(gulp.dest('build'))
 });
 
-gulp.task('annotate', function(){
-    gulp.src('build/railroadRoute.js')
-        .pipe(ngAnnotate())
-        .pipe(gulp.dest(paths.dest[0]));
-    gulp.src('build/dependencies.js')
+gulp.task('dep-annotate', function(){
+    return gulp.src('build/dependencies.js')
         .pipe(ngAnnotate())
         .pipe(gulp.dest(paths.dest[0]))
 });
 
-gulp.task('clean', function(){
-    gulp.src(paths.clean[0])
-        .pipe(clean());
-    gulp.src(paths.clean[1])
-        .pipe(clean());
-});
-
-gulp.task('copy', function(){
-    gulp.src('build/dependencies.js')
-        .pipe(gulp.dest(paths.dest[0]));
-    gulp.src('build/railroadRoute.js')
+gulp.task('src-annotate', function(){
+    return gulp.src('build/railroadRoute.js')
+        .pipe(ngAnnotate())
         .pipe(gulp.dest(paths.dest[0]));
 });
 
+gulp.task('build-clean', function(){
+    return gulp.src(paths.clean[0])
+        .pipe(clean());
+});
 
-gulp.task('default', ['template', 'build', 'annotate', 'copy']);
+gulp.task('web-app-clean', function(){
+    return gulp.src(paths.clean[1])
+        .pipe(clean());
+});
+
+gulp.task('dep-copy', function(){
+    return gulp.src('build/dependencies.js')
+        .pipe(gulp.dest('web-app/js'));
+});
+
+gulp.task('src-copy', function(){
+    return gulp.src('build/railroadRoute.js')
+        .pipe(gulp.dest('web-app/js'));
+});
+
+
+gulp.task('default', function(callback){
+    runSequence(['web-app-clean','build-clean'],
+                'template',
+                ['src-build','dep-build'],
+                ['src-annotate','dep-annotate'],
+                ['src-copy','dep-copy'],
+                callback)
+});
